746155e5cca8634982e03605578175fbe23dc573
Neha Tomar <tneha5161@gmail.com>
2022-03-25 22:15:44 +0530

    background color changed

--- id_card.js
+++ id_card.js
@@ -1,11 +1,13 @@
 function details() {
   document.querySelector(".sId").innerHTML = localStorage.getItem("sId");
 
-  document.querySelector(".name").innerHTML = localStorage.getItem("name");
+  document.querySelector(".name__value").innerHTML =
+    localStorage.getItem("name");
 
-  document.querySelector(".branch").innerHTML = localStorage.getItem("branch");
+  document.querySelector(".branch_name").innerHTML =
+    localStorage.getItem("branch");
 
-  document.querySelector(".section").innerHTML =
+  document.querySelector(".your_section").innerHTML =
     localStorage.getItem("section");
 }
 window.addEventListener("DOMContentLoaded", details);
